41bd42f8f08cf80d2de11d89c77bf0b0b6f0dc2d
arafateouronile-glitch <arafateouronile@gmail.com>
2025-11-09 23:49:52 +0100

    fix: ajout import Link manquant dans ProfilePage

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import {
   User,
   Mail,
